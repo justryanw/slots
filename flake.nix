f02b9b3b3cf8543e171472f4494ec8136cd95998
Ryan Walker <ryanjwalker2001@gmail.com>
2024-11-13 15:13:29 +0000

    extensions

--- flake.nix
+++ flake.nix
@@ -3,16 +3,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            deno
-          ];
+          packages = {
+            inherit (pkgs) deno;
+          };
         };
       }
     );
