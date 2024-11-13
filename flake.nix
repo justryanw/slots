c060fc7781e09a97f3fbd0b05192649084552336
Ryan Walker <ryanjwalker2001@gmail.com>
2024-11-13 15:51:49 +0000

    deno overlay

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    deno-overlay.url = "github:haruki7049/deno-overlay";
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      deno-overlay,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ deno-overlay.overlays.deno-overlay ];
+        };
+        deno-version = "2.0.6";
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = {
-            inherit (pkgs) deno;
-          };
+          packages = with pkgs; [
+            deno.${deno-version}
+          ];
         };
       }
     );
--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
+import { generate } from "https://esm.sh/randomstring";
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Add 2 + 3 =", add(2, 3), generate());
 }
